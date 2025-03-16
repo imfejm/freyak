b6f8e63a86ec3d8b2ba2ae22e7ae957bf67c5420
imfejm <4imfejm@gmail.com>
2025-03-16 20:55:54 +0100

    navigation edit

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ tlacitko.addEventListener("click", () => {
 
 //navigace
 document.addEventListener("DOMContentLoaded", function () {
-  const navLink = document.getElementById("nav-link");
-  const navText = document.getElementById("nav-text");
-  let firstClick = false;
-  const navUrl = "geo:50.3759308,15.5746725"; // Správný odkaz na navigaci
+    const navLink = document.getElementById("nav-link");
+    const navText = document.getElementById("nav-text");
+    let firstClick = false;
+    const navUrl = "geo:50.3759308,15.5746725"; // Správný odkaz na navigaci
 
-  navLink.addEventListener("click", function (event) {
-    if (!firstClick) {
-      event.preventDefault(); // Zabrání okamžitému přesměrování
-      navText.style.display = "inline"; // Zobrazí text
-      firstClick = true;
-    } else {
-      window.location.href = navUrl; // Otevře navigaci
-    }
-  });
+    navLink.addEventListener("click", function (event) {
+        if (!firstClick) {
+            event.preventDefault(); // Zabrání okamžitému přesměrování
+            navText.style.display = "inline"; // Zobrazí text
+            firstClick = true;
+            setTimeout(() => {
+                navLink.href = navUrl; // Nastaví správné href
+            }, 10); // Po krátké prodlevě zajistí správné přesměrování
+        }
+    });
 });
