19bd1a49999f6f99f84ded8e977bebcb3e18e4b9
imfejm <4imfejm@gmail.com>
2025-03-17 14:21:07 +0100

    forms edit

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+//navigace na klik a krizek z ham
 const tlacitko = document.querySelector("#ham");
+const rozbal = document.querySelector("#menu");
+const odkazy = rozbal.querySelectorAll("a");
 
 tlacitko.addEventListener("click", () => {
-  const rozbal = document.querySelector("#menu");
   rozbal.classList.toggle("hidden");
-  const cara2 = document.querySelector("#cara2");
-  cara2.classList.toggle("caraB");
 
-  const cara1 = document.querySelector("#cara1");
-  cara1.classList.toggle("caraA");
-  const cara3 = document.querySelector("#cara3");
-  cara3.classList.toggle("caraC");
+  document.querySelector("#cara1").classList.toggle("caraA");
+  document.querySelector("#cara2").classList.toggle("caraB");
+  document.querySelector("#cara3").classList.toggle("caraC");
+});
+
+// Zavření menu po kliknutí na jakýkoli odkaz
+odkazy.forEach(link => {
+  link.addEventListener("click", () => {
+    rozbal.classList.add("hidden"); // Skryje menu
+
+    // Vrátí čáry do původní polohy (křížek zpět na hamburger)
+    document.querySelector("#cara1").classList.remove("caraA");
+    document.querySelector("#cara2").classList.remove("caraB");
+    document.querySelector("#cara3").classList.remove("caraC");
+  });
 });
 
 //navigace
