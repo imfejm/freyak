3e04ac314c5a1c2452cbbb548f59ba3ae483863a
imfejm <4imfejm@gmail.com>
2025-03-17 13:15:37 +0100

    navigation edit

--- index.js
+++ index.js
@@ -17,26 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const navLink = document.getElementById("nav-link");
   const navText = document.getElementById("nav-text");
 
-  // GPS souřadnice pro adresu Holovousy 53
-  const lat = "50.3759308";
-  const lon = "15.5746725";
-
-  // Univerzální odkaz pro mobilní Google Maps
-  const googleMapsNav = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lon}&travelmode=driving`;
-
-  navLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Zabrání okamžitému přesměrování
-
-    // Pokud je text zobrazený => uživatel už klikl dříve, rovnou přesměruj
-    if (navText.style.display === "inline") {
-      window.location.href = googleMapsNav;
-    } else {
-      navText.style.display = "inline"; // Zobrazí text
-      setTimeout(() => {
-        navLink.href = googleMapsNav; // Nastaví odkaz
-      }, 10);
-    }
-  });
+  // Text zobrazíme hned po načtení stránky
+  navText.style.display = "inline";
 });
-
-
